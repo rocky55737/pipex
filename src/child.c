36fa8e2c3a62ae9bb3f96ae65c8a30f95717e3c5
unknown <rocky55737@gmail.com>
2022-09-24 18:55:11 +0900

    child

--- src/child.c
+++ src/child.c
@@ -15,6 +15,8 @@
 void			child(t_child_data	c_data);
 t_child_data	child_data_set(int *pids, int pipe_cnt, t_pipe_data *p_data, \
 int pipes[2][2]);
+static char 	*cmd_path_find(char *cmd, char **environ);
+static void	io_dup(int *in_out_fd);
 
 void	child(t_child_data	c_data)
 {
@@ -27,7 +29,11 @@ void	child(t_child_data	c_data)
 	cmd_path = cmd_path_find(command[1], c_data.p_d->environ);
 	io_dup(c_data.fd_in_out);
 	fd_arr_close(c_data.fd_to_close);
-	execve(cmd_path, command, 0);
+	if (execve(cmd_path, command, c_data.p_d->environ) == -1)
+	{
+		perror("execve error");
+		exit(1);
+	}
 }
 
 t_child_data	child_data_set(int *pids, int pipe_cnt, t_pipe_data *p_data, \
@@ -50,9 +56,39 @@ int pipes[2][2])
 	return (c_data);
 }
 
-char *cmd_path_find(char *cmd, char **environ)
+static char *cmd_path_find(char *cmd, char **environ)
 {
+	char	**paths_env;
+	int		cnt;
+
 	if (access(cmd, X_OK) == 0)
 		return (cmd);
-	else 
+	while (ft_strncmp(*environ, "PATH=", 5) != 0)
+		environ++;
+	paths_env = ft_split(&((*environ)[5]), ':');
+	cnt = 0;
+	while(paths_env[cnt])
+		paths_env[cnt] = ft_strjoin(paths_env[cnt], "/");
+	while (*paths_env | access(ft_strjoin(*paths_env, cmd), X_OK) != 0)
+		paths_env++;
+	if (paths_env == 0)
+	{
+		perror("command not found");
+		exit(1);
+	}
+	return (ft_strjoin(*paths_env, cmd));
+}
+
+static void	io_dup(int *in_out_fd)
+{
+	if (dup2(in_out_fd[0], 0) == -1)
+	{
+		perror("dup2 error");
+		exit(1);
+	}
+	if (dup2(in_out_fd[1], 1) == -1)
+	{
+		perror("dup2 error");
+		exit(1);
+	}
 }
